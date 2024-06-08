94917774dbc9ca80da0a8f2d20adb989e702f96e
Ibrahim1249 <mominibrahim98.im85@gmail.com>
2024-06-08 14:25:02 +0530

    working on css

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -9,6 +9,10 @@ function Detail() {
         <p>Home Value</p>
         <p>$ 1000</p>
         <DiscreteSlider />
+        <div className="change">
+            <p>$1000</p>
+            <p>$10000</p>
+        </div>
       </div>
     </>
   );
